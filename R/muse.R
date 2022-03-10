fe4c34d0b72a91ef648bbce713a71a971e934e2a
David Keyes <david@rfortherestofus.com>
2022-03-10 10:19:08 -0800

    Update various bit of muse stuff

--- R/muse.R
+++ R/muse.R
@@ -1,16 +1,30 @@
 #' Upload video to muse.ai
 #'
 #' @param local_video Location of local video
+#' @param video_title What to call video on muse
 #'
 #' @return
 #' @export
 #'
 #' @examples
-upload_muse_video <- function(local_video) {
+upload_muse_video <- function(local_video, video_title) {
 
-  httr2::request("https://muse.ai/api/files/upload") %>%
+  uploaded_video_info <- httr2::request("https://muse.ai/api/files/upload") %>%
     httr2::req_headers("Key" = Sys.getenv("MUSE_API_KEY")) %>%
     httr2::req_body_multipart(list(file = curl::form_file(local_video))) %>%
+    httr2::req_perform()
+
+  latest_video_id <- uploaded_video_info %>%
+    httr2::resp_body_json() %>%
+    tibble::as_tibble() %>%
+    dplyr::mutate(video_id = stringr::str_glue("https://muse.ai/api/files/set/{fid}")) %>%
+    dplyr::pull(video_id)
+
+  httr2::request(latest_video_id) %>%
+    httr2::req_headers("Key" = Sys.getenv("MUSE_API_KEY"),
+                       "Content-Type" = "application/json") %>%
+    httr2::req_body_json(list(title = video_title,
+                              visibility = "unlisted")) %>%
     httr2::req_perform()
 
 }
@@ -42,19 +56,6 @@ download_latest_zoom_video <- function() {
 
 }
 
-#' Post the latest Zoom video to muse
-#'
-#' @return
-#' @export
-#'
-#' @examples
-post_zoom_video_to_muse <- function() {
-
-  download_latest_zoom_video() %>%
-    upload_muse_video()
-}
-
-
 #' Download video from Cleanshot
 #'
 #' @param cleanshot_url
@@ -64,9 +65,9 @@ post_zoom_video_to_muse <- function() {
 #' @export
 #'
 #' @examples
-download_cleanshot_video <- function(cleanshot_url, file_name) {
+download_cleanshot_video <- function(cleanshot_url) {
 
-  video_location <- stringr::str_glue("/users/davidkeyes/Downloads/{file_name}.mp4")
+  video_location <- stringr::str_glue("{tempdir()}video.mp4")
 
   download.file(stringr::str_glue("{cleanshot_url}/download"),
                 destfile = video_location)
@@ -74,4 +75,5 @@ download_cleanshot_video <- function(cleanshot_url, file_name) {
   video_location
 
 }
+
 
