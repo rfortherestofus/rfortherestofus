fdf7e1fa2f4d826c8b4fcf43464adeb23f425a69
Albert Rapp <AlbertRapp@gmx.de>
2025-09-29 20:48:06 +0200

    Deprecate the load_obtn_data() function

--- R/load_obtn_data.R
+++ R/load_obtn_data.R
@@ -9,38 +9,9 @@
 #' @return A character string representing the path to the downloaded file, or `NULL` if the download fails.
 #' @export
 download_file <- function(file_path, dest_path, verbose = FALSE) {
-    if (!dir.exists(dest_path)) {
-        message("Creating temporary directory: ", dest_path)
-        dir.create(dest_path, recursive = TRUE)
-    }
-
-    response <- tryCatch(
-        {
-            gh::gh(
-                "GET /repos/{owner}/{repo}/contents/{path}",
-                owner = "rfortherestofus",
-                repo = "obtn_pipeline",
-                path = file_path
-            )
-        },
-        error = function(e) {
-            warning("Error fetching file '", file_path, "': ", e$message)
-            return(NULL)
-        }
-    )
-
-    if (is.null(response)) return(NULL)
-
-    file_content <- base64enc::base64decode(response$content)
-    file_name <- basename(file_path)
-    dest_file <- file.path(dest_path, file_name)
-    writeBin(file_content, dest_file)
-
-    if (verbose) {
-        message("File saved to: ", dest_file)
-    }
-
-    return(dest_file)
+  stop(
+    'load_obtn_data() and related functionality has been moved to the {tfff} package'
+  )
 }
 
 #' Get a list of files matching a pattern from a GitHub repository
@@ -53,33 +24,9 @@ download_file <- function(file_path, dest_path, verbose = FALSE) {
 #' @return A character vector of file paths that match the pattern, or an error if the directory listing fails.
 #' @export
 get_files <- function(pattern, dir = "data-clean/") {
-    pattern <- paste0(dir, pattern)
-
-    dir_contents <- tryCatch(
-        {
-            gh::gh(
-                "GET /repos/{owner}/{repo}/contents/{path}",
-                owner = "rfortherestofus",
-                repo = "obtn_pipeline",
-                path = dirname(pattern)
-            )
-        },
-        error = function(e) {
-            stop("Error listing directory: ", e$message)
-        }
-    )
-
-    file_pattern <- basename(pattern)
-    matching_files <- sapply(dir_contents, function(x) {
-        if (grepl(file_pattern, x$name) && x$type == "file") {
-            return(x$path)
-        } else {
-            return(NULL)
-        }
-    })
-
-    matching_files <- unlist(matching_files)
-    return(matching_files)
+  stop(
+    'load_obtn_data() and related functionality has been moved to the {tfff} package'
+  )
 }
 
 #' Load data from a GitHub repository
@@ -99,35 +46,12 @@ get_files <- function(pattern, dir = "data-clean/") {
 #' }
 #' @export
 load_obtn_data <- function(
-    pattern,
-    dest_path = "temp/",
-    verbose = FALSE,
-    delete_temp_dir = TRUE
+  pattern,
+  dest_path = "temp/",
+  verbose = FALSE,
+  delete_temp_dir = TRUE
 ) {
-    all_files <- get_files(pattern = pattern)
-    for (file in all_files) {
-        download_file(
-            file_path = file,
-            dest_path = dest_path,
-            verbose = verbose
-        )
-    }
-
-    df <- purrr::map_dfr(
-        list.files(dest_path, pattern = pattern, full.names = TRUE),
-        function(file) {
-            obj <- qs::qread(file)
-            # If it's a list of one element (e.g., list(data_alice = <df>)), extract the first element
-            if (is.list(obj) && length(obj) == 1 && is.data.frame(obj[[1]])) {
-                return(obj[[1]])
-            } else {
-                return(obj)
-            }
-        }
-    )
-
-    if (delete_temp_dir) {
-        unlink(dest_path, recursive = TRUE)
-    }
-    return(df)
+  stop(
+    'load_obtn_data() and related functionality has been moved to the {tfff} package'
+  )
 }
