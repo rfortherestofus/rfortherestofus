65e9782a8e7473e4f08aa05afc19efada0d48da8
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2026-02-03 21:53:12 +0100

    rename AGENT.md --> AGENTS.md

--- R/agents_md.R
+++ R/agents_md.R
@@ -77,30 +77,30 @@ You can find syntax information by reading this page (https://typst.app/docs/ref
 "
 )
 
-#' @title Create or overwrite an AGENT.md configuration file
+#' @title Create or overwrite an AGENTS.md configuration file
 #'
-#' @description Generates an `AGENT.md` file with common code practices
+#' @description Generates an `AGENTS.md` file with common code practices
 #' (R, Quarto) for LLMs. By default, writes a new file unless `overwrite = TRUE`
 #' is specified.
-#' You basically create the file with `rfortherestofus::use_agent_md()` and then
+#' You basically create the file with `rfortherestofus::use_agents_md()` and then
 #' tell your favorite AI to read it. You can also copy paste the content to use in
 #' other interface.
 #'
-#' @param sections Character vector with sections to add in the `AGENT.md`. Possible
+#' @param sections Character vector with sections to add in the `AGENTS.md`. Possible
 #' values are "R" and "Quarto".
 #' @param overwrite Logical. Whether to overwrite an existing file. Default is `FALSE`.
 #'
 #' @examples
 #' \dontrun{
-#' rfortherestofus::use_agent_md("R")              # just R rules
-#' rfortherestofus::use_agent_md("Quarto")         # just Quarto rules
-#' rfortherestofus::use_agent_md("Typst")          # just Typst rules
-#' rfortherestofus::use_agent_md(c("R", "Quarto")) # R and Quarto rules
+#' rfortherestofus::use_agents_md("R")              # just R rules
+#' rfortherestofus::use_agents_md("Quarto")         # just Quarto rules
+#' rfortherestofus::use_agents_md("Typst")          # just Typst rules
+#' rfortherestofus::use_agents_md(c("R", "Quarto")) # R and Quarto rules
 #' }
 #'
 #' @export
-use_agent_md <- function(sections, overwrite = FALSE) {
-  file_name <- "AGENT.md"
+use_agents_md <- function(sections, overwrite = FALSE) {
+  file_name <- "AGENTS.md"
   if (file.exists(file_name) && !overwrite) {
     stop(paste0(
       file_name,
